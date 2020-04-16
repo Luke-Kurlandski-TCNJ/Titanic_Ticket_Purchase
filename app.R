d7f44c23a1f09e48cd3ac73f366ae7db794c4505
Luke Kurlandski <kurlanl1@tcnj.edu>
2020-04-16 19:02:07 -0400

    Fully functioning single graph of survival vs variable

--- app.R
+++ app.R
@@ -5,21 +5,89 @@ library(lubridate)
 library(ggplot2)
 library(shiny)
 library(tidyr)
+library(tibble)
+#library(naniar)
 
 theme_set(theme_bw())
 
-# Data set of all passengers on the titanic
-train_set <- read.csv("datasets/train.csv")
-#View(train_set)
-#print(is.na(train_set))
-#print(anyNA(train_set))
-print(train_set$Embarked)
+# Apply an age category to an age.
+determine_age_category <- function(age) {
+    if (is.na(age) || age=="")
+        return(NA)
+    else
+        return(age %/% 5)
+}
 
-# Separate based upon the type of data. Remove irrelevant columns.
-train_set_categorical <- train_set %>%
-    select(Survived, Pclass, Sex, Ticket, Cabin, Embarked) #FIXME: examine cabin and ticket
-train_set_numerical <- train_set %>%
-    select(Survived, Age, SibSp, Parch, Fare)
+# Apply a fare category to a fare.
+determine_fare_category <- function(fare) {
+  if (is.na(fare) || fare=="")
+    return(NA)
+  else
+    return(fare %/% 10)
+}
+
+# Determine whether a passeneger embarked alone or not.
+determine_alone <- function(parents_children, siblings_spouses) {
+    if (parents_children == 0 && siblings_spouses == 0)
+        return(TRUE)
+    else 
+        return(FALSE)
+}
+
+# Data set of all passengers on the titanic.
+train_set <- read.csv("datasets/train.csv") %>% 
+    #replace_with_na_all(condition = ~.x == "")
+    mutate(Alone = mapply(determine_alone, SibSp, Parch)) %>%
+    mutate(AgeCat = mapply(determine_age_category, Age)) %>%
+    mutate(FareCat = mapply(determine_fare_category, Fare))
+View(train_set)
+# Separate based upon the type of data.
+variables_categorical <- c("Pclass", "Sex", "Ticket", 
+                           "Cabin", "Embarked", "Cabin",
+                           "Ticket", "AgeCat", "Alone",
+                           "SibSp", "Parch", "FareCat")
+vars_to_colnames <- c("Sex"="Sex", "Age" = "AgeCat", 
+                     "Number of Siblings/Spouses" = "SibSp", 
+                     "Number of Parents/Children" = "Parch",
+                     "Lone Traveller" = "Alone",
+                     "Ticket" = "Ticket", #FIXME: implement or remove
+                     "Fare" = "FareCat", "Cabin" = "Cabin", #FIXME: implement or remove
+                     "Port Embarked From" = "Embarked")
+vars_to_colnames
+
+display_vars <- c("Sex", "Number of Siblings/Spouses", "Number of Parents/Children",
+                  "Ticket", "Cabin", "Port Embarked From", "Lone Traveller", "Age", "Fare")
+data_vars <- colnames(select(train_set, -PassengerId, -Survived, -Age, -Name, -Fare))
+
+
+# Output a chart when a categorical variable is selected.
+categorical_chart <- function(variable, which_plot) {
+    # Determine statistics for each group of the variable.
+    survival_stats <- train_set %>%
+        select(variable, Survived) %>%
+        filter(complete.cases(.), !!as.name(variable) != "") %>%
+        group_by_at(variable) %>%
+        summarize(total=n(), 
+                  totalSurvived=sum(Survived==1), 
+                  totalDead=total-totalSurvived,
+                  survivalProbability=totalSurvived/total,
+                  deathProbability=totalDead/total)
+    View(survival_stats)
+    # Colors for the graph.
+    colors <- c("Total" = "red1", "Survived" = "orange1")
+    p <- ggplot(survival_stats) +
+        geom_bar(aes(x=!!as.name(variable), y=total, fill="Total"), 
+                 stat="identity", position="dodge")    + 
+        geom_bar(aes(x=!!as.name(variable), y=totalSurvived, fill="Survived"), 
+                stat="identity", position="dodge")   + 
+        geom_text(aes(x=!!as.name(variable), y=total, label=total), 
+                  position=position_dodge(width=0.9), vjust=-0.25) +
+        geom_text(aes(x=!!as.name(variable), y=totalSurvived, label=totalSurvived), 
+                  position=position_dodge(width=0.9), vjust=-0.25) +
+        labs(x=display_vars[match(variable, vars_to_colnames)], y="Number of Passengers", color = "Legend")  +
+        scale_fill_manual(name="Legend", values = colors)
+    print(p)
+}
 
 # UI aspect of shiny.
 ui <- fluidPage(
@@ -29,15 +97,23 @@ ui <- fluidPage(
     sidebarLayout(
         # Side bar to display user selection.
         sidebarPanel(
-            # User selects which aspects to plot examine.
-            selectizeInput(inputId="aspect", 
-                           label="Plot Survival vs Aspect",
-                           choices=colnames(select(train_set, -Survived, -PassengerId)),
+            # User selects which variables to plot examine.
+            selectizeInput(inputId="variable", 
+                           label="Plot Survival vs Variable",
+                           choices=c("Sex"="Sex",
+                                    "Age" = "AgeCat",
+                                    "Number of Siblings/Spouses" = "SibSp",
+                                    "Number of Parents/Children" = "Parch",
+                                    "Lone Traveller" = "Alone",
+                                    "Ticket" = "Ticket", #FIXME: implement or remove
+                                    "Fare" = "FareCat",
+                                    "Cabin" = "Cabin", #FIXME: implement or remove
+                                    "Port Embarked From" = "Embarked"),
                            selected=c("Pclass"),
                            multiple=FALSE),
-            # User selects how to examine selected aspect.
+            # User selects how to examine selected variable.
             selectizeInput(inputId="which_plot", 
-                           label="Associated Survival Plots w Aspect", 
+                           label="Associated Survival Plots w Variable", 
                            choices=c("Total" = "total", 
                                      "Total Survived" = "totalSurvived", 
                                      "Total Dead" = "totalDead",
@@ -49,43 +125,19 @@ ui <- fluidPage(
         
         # Main panel to display graphs.
         mainPanel(
-            plotOutput("aspect_vs_survival")
+            plotOutput("variable_vs_survival")
         )
     )
 )
 
-# Server aspect of shiny.
+# Server variable of shiny.
 server <- function(input, output) {
     
-    output$aspect_vs_survival <- renderPlot({
-        # The data user selects to graph.
-        aspect <- input$aspect
-        which_plot <- input$which_plot
+    output$variable_vs_survival <- renderPlot({
+
         # Handle a graph for a categorical variable.
-        if (aspect %in% colnames(train_set_categorical))
-            # Select the associated aspect, and Survived columns.
-            set <- select(train_set_categorical, aspect, Survived)
-            # Determine statistics for each group.
-            set <- set %>%
-                # Drop NA and "" values.
-                filter(complete.cases(.), !!as.name(aspect) != "") %>%
-                # Group by the relevant aspect.
-                group_by_at(aspect) %>%
-                # Generate a summary table of survival information.
-                summarize(total=n(), 
-                          totalSurvived=sum(Survived==1), 
-                          totalDead=total-totalSurvived,
-                          survivalProbability=totalSurvived/total,
-                          deathProbability=totalDead/total)
-            View(set)
-            # Output the graph.
-            p <- ggplot(set, aes(x=!!as.name(aspect), y=!!as.name(which_plot))) +
-              geom_bar(stat="identity") +
-              labs(x=aspect, y=which_plot)
-            print(p)
-            
-        if (aspect %in% colnames(train_set_numerical))
-            set <- train_set_numerical
+        if (input$variable %in% variables_categorical)
+            categorical_chart(input$variable, input$which_plot)
     })
 }
 
